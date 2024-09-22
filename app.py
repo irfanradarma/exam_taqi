23f6900665ec7007b8501293addb7d22b7b44148
Irfan Radarma <102050150+irfanradarma@users.noreply.github.com>
2024-09-22 15:36:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ if "PKN" not in st.session_state:
     st.session_state.PKN = pd.read_csv("https://docs.google.com/spreadsheets/d/13PriSfxskOTcAlGv-gFd7WoiB2nQ2yx9hzsYQQUqJeY/export?format=csv&gid=295218205")
 if "BINDO" not in st.session_state:
     st.session_state.BINDO = pd.read_csv("https://docs.google.com/spreadsheets/d/13PriSfxskOTcAlGv-gFd7WoiB2nQ2yx9hzsYQQUqJeY/export?format=csv&gid=986118028")
+if "AKIDAH" not in st.session_state:
+    st.session_state.AKIDAH = pd.read_csv("https://docs.google.com/spreadsheets/d/13PriSfxskOTcAlGv-gFd7WoiB2nQ2yx9hzsYQQUqJeY/export?format=csv&gid=1866255283")
 
 def show_soal(subject, subject_key):
     if "submit" not in st.session_state:
@@ -45,7 +47,7 @@ def show_soal(subject, subject_key):
 
 def main():
     st.title("Latihan Ulangan")
-    tab_mtk, tab_pkn, tab_bindo = st.tabs(["Matematika", "PKN", "B. Indonesia"])
+    tab_mtk, tab_pkn, tab_bindo, tab_akidah = st.tabs(["Matematika", "PKN", "B. Indonesia", "Akidah"])
     with tab_mtk:
         nilai, jml_soal = show_soal(st.session_state.MTK, "mtk")
         st.write(f"Jumlah betul: {nilai}")
@@ -54,6 +56,34 @@ def main():
         nilai, jml_soal = show_soal(st.session_state.PKN, 'pkn')
         st.write(f"Jumlah betul: {nilai}")
         st.write(f"Nilainya adalah {nilai/jml_soal * 100:.2f}%")
+    with tab_bindo:
+        subtab1, subtab2, subtab3 = st.tabs(["Satu", "Dua", "Tiga"])
+        with subtab1:
+            nilai, jml_soal = show_soal(st.session_state.BINDO[st.session_state.BINDO["No."].isin(range(1,11))], "bindo1")
+            st.write(f"Jumlah betul: {nilai}")
+            st.write(f"Nilainya adalah {nilai/jml_soal * 100:.2f}%")
+        with subtab2:
+            nilai, jml_soal = show_soal(st.session_state.BINDO[st.session_state.BINDO["No."].isin(range(11,21))], "bindo2")
+            st.write(f"Jumlah betul: {nilai}")
+            st.write(f"Nilainya adalah {nilai/jml_soal * 100:.2f}%")
+        with subtab3:
+            nilai, jml_soal = show_soal(st.session_state.BINDO[st.session_state.BINDO["No."].isin(range(21,31))], "bindo3")
+            st.write(f"Jumlah betul: {nilai}")
+            st.write(f"Nilainya adalah {nilai/jml_soal * 100:.2f}%")
+    with tab_akidah:
+        subtab1, subtab2, subtab3 = st.tabs(["Satu", "Dua", "Tiga"])
+        with subtab1:
+            nilai, jml_soal = show_soal(st.session_state.AKIDAH[st.session_state.AKIDAH["No."].isin(range(1,11))], "akidah1")
+            st.write(f"Jumlah betul: {nilai}")
+            st.write(f"Nilainya adalah {nilai/jml_soal * 100:.2f}%")
+        with subtab2:
+            nilai, jml_soal = show_soal(st.session_state.AKIDAH[st.session_state.AKIDAH["No."].isin(range(11,21))], "akidah2")
+            st.write(f"Jumlah betul: {nilai}")
+            st.write(f"Nilainya adalah {nilai/jml_soal * 100:.2f}%")
+        with subtab3:
+            nilai, jml_soal = show_soal(st.session_state.AKIDAH[st.session_state.AKIDAH["No."].isin(range(21,31))], "akidah3")
+            st.write(f"Jumlah betul: {nilai}")
+            st.write(f"Nilainya adalah {nilai/jml_soal * 100:.2f}%")
 
 if __name__ == "__main__":
     main()
